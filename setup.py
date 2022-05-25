f237e487a143add437559aa7681bf08ca3c87def
ragibson <ryan.alexander.gibson@gmail.com>
2022-05-25 10:22:18 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author="Ryan Gibson",
     author_email="ryanalexandergibson@gmail.com",
     name="stego_lsb",
-    version="1.3.3",
+    version="1.3.4",
     description="stego lsb",
     keywords="stego lsb",
     license="MIT",
